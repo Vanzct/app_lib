ef06cad6bfd488e83a976f2f74600eaca67c94f4
张寻 <heidazhangchengtao@163.com>
2015-10-19 13:43:58 +0800

    manager

--- manage.py
+++ manage.py
@@ -1,24 +1,25 @@
 __author__ = 'Van'
 
 import os
-# from flask.ext.script import Manager, Shell
+from flask.ext.script import Manager, Shell
 # from flask.ext.migrate import Migrate, MigrateCommand
 from app import create_app
 
 app = create_app(os.getenv('APP_LIB_CONFIG') or 'default')
-# manager = Manager(app)
+manager = Manager(app)
 
 
 # manager.add_command("shell", Shell(make_context=make_shell_context))
 # manager.add_command('db', MigrateCommand)
 
-# @manager.command
-# def test():
-#     """aRun the unit tests."""
-#     import unittest
-#     tests = unittest.TestLoader().discover('tests')
-#     unittest.TextTestRunner(verbosity=2).run(tests)
+@manager.command
+def test():
+    """aRun the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    # app.run(host='0.0.0.0')
+    manager.run()
