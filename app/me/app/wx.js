0cbac36d02be2e0948b958b3af8c1fb346c87efb
张寻 <heidazhangchengtao@163.com>
2015-11-14 01:23:53 +0800

    wx sign

--- app/me/app/wx.js
+++ app/me/app/wx.js
@@ -1,11 +1,11 @@
-wx.config({
-    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-    appId: 'wx1d5a93e16a3a77e2', // 必填，公众号的唯一标识
-    timestamp: , // 必填，生成签名的时间戳
-    nonceStr: '', // 必填，生成签名的随机串
-    signature: '',// 必填，签名，见附录1
-    jsApiList: [] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-});
+//wx.config({
+//    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+//    appId: 'wx1d5a93e16a3a77e2', // 必填，公众号的唯一标识
+//    timestamp: , // 必填，生成签名的时间戳
+//    nonceStr: '', // 必填，生成签名的随机串
+//    signature: '',// 必填，签名，见附录1
+//    jsApiList: [] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+//});
 wx.onMenuShareAppMessage({
     title: '张寻的简历', // 分享标题
     desc: '描述,这是最好的时代', // 分享描述
